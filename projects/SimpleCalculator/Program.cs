52e9b16e162353586cf79cb2e59bebdd9d9baaa3
Prem Kumar <premchinna94@gmail.com>
2026-07-15 20:54:28 +0530

    simple calculator

---
+++ projects/SimpleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<====== Simple Calculator ==========>");
+            Console.WriteLine("Enter the whole number");
+            int number1 = 0;
+            number1  = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the whole number");
+            int number2 = 0;
+            number2 = int.Parse(Console.ReadLine());
+
+            int total = number1 + number2;
+            Console.WriteLine("The sum of" + number1 + " + " + number2 + " is " + total);
+            Console.ReadKey();
+        }
+    }
+}
